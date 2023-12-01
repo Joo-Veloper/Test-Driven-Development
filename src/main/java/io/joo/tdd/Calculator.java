c6c1db82e36e50cdf482ace3ec9eaccefac9260d
Joo-Veloper <psjoo3515@gmail.com>
2023-12-01 15:58:55 +0900

    Calculator 테스트

---
+++ src/main/java/io/joo/tdd/Calculator.java
@@ -0,0 +1,10 @@
+package io.joo.tdd;
+
+public class Calculator {
+//    public static int plus(int a1, int a2){
+//        return 3;
+//    }
+    public static int plus(int a1, int a2) {
+        return a1 + a2;
+    }
+}
---
+++ src/test/java/io/joo/tdd/CalculatorTest.java
@@ -0,0 +1,19 @@
+package io.joo.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+//    @Test
+//    void plus(){
+//        int result = Calculator.plus(1,2);
+//        assertEquals(3,result);
+//    }
+    @Test
+    void plus() {
+        int sum = Calculator.plus(1,2);
+        assertEquals(3, sum);
+        assertEquals(5,Calculator.plus(4,1));
+    }
+}
