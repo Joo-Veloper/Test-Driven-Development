3864315e4a1871e4d9064e23868c0f8c74935a2f
Seung Joo <psjoo3515@gmail.com>
2024-07-12 16:11:23 +0900

    카드 번호 테스트

---
+++ src/test/java/io/joo/tdd/treason/AutoDebitRegisterTest.java
@@ -0,0 +1,37 @@
+package io.joo.tdd.treason;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.joo.tdd.treason.CardValidity.THEFT;
+import static io.joo.tdd.treason.CardValidity.VALID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AutoDebitRegisterTest {
+    private AutoDebitRegister register;
+
+    @BeforeEach
+    void setUp() {
+        CardNumberValidator validator = new CardNumberValidator();
+        AutoDebitInfoRepository repository = new JpaAutoDebitInfoRepository();
+        register = new AutoDebitRegister(validator, repository);
+    }
+
+    @Test
+    @DisplayName("업체에서 받은 테스트용 유효한 카드 번호 사용")
+    void validCard() {
+        AutoDebitReq req = new AutoDebitReq("user1", "1234123412341234");
+        RegisterResult result = this.register.register(req);
+        assertEquals(VALID, result.getValidity());
+    }
+
+    @Test
+    @DisplayName("업체에서 받은 도난 테스트용 카드 번호 사용")
+    void theftCard() {
+        AutoDebitReq req = new AutoDebitReq("user1", "1234567890123456");
+        RegisterResult result = this.register.register(req);
+        assertEquals(THEFT, result.getValidity());
+    }
+}
