786054c7845aa26f7f0dbfd0a19f1b0d5d7bd8b3
Seung Joo <psjoo3515@gmail.com>
2024-07-22 23:29:39 +0900

    BaseBallGame DataFile Test

---
+++ src/test/java/io/joo/tdd/baseball/game/BaseBallGameTest.java
@@ -0,0 +1,12 @@
+package io.joo.tdd.baseball.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseBallGameTest {
+    @Test
+    void allMatch(){
+        BaseBallGame game = new BaseBallGame("456");
+    }
+}
