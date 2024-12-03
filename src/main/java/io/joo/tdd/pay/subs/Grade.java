22aa0f66b53a3205ca2e576e9af85ca829d5a192
Seung Joo <psjoo3515@gmail.com>
2024-12-03 21:34:40 +0900

    Subscription Grade and Exception

---
+++ src/main/java/io/joo/tdd/pay/subs/Grade.java
@@ -0,0 +1,5 @@
+package io.joo.tdd.pay.subs;
+
+public enum Grade {
+    GOLD
+}
---
+++ src/main/java/io/joo/tdd/pay/subs/NoSubscriptionException.java
@@ -0,0 +1,7 @@
+package io.joo.tdd.pay.subs;
+
+public class NoSubscriptionException extends RuntimeException {
+    public NoSubscriptionException() {
+
+    }
+}
