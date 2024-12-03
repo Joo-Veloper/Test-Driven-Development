d9f3f1a71416f61ae71b4ef02baaf61842ad51a2
Seung Joo <psjoo3515@gmail.com>
2024-12-03 21:59:17 +0900

    update AuthService

---
+++ src/main/java/io/joo/tdd/pay/auth/AuthService.java
@@ -0,0 +1,14 @@
+package io.joo.tdd.pay.auth;
+
+public class AuthService {
+    private String authKey = "somekey";
+
+    public int authenticate(String id, String pw) {
+        boolean authorized = AuthUtil.authorize(authKey);
+        if (authorized) {
+            return AuthUtil.authenticate(id, pw);
+        } else {
+            return -1;
+        }
+    }
+}
