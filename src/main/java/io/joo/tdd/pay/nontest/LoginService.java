51d3e31e2a0cfdac2829aa85c2996ae9f84f81de
Seung Joo <psjoo3515@gmail.com>
2024-12-03 21:55:02 +0900

    update LoginService

--- src/main/java/io/joo/tdd/pay/nontest/LoginService.java
+++ src/main/java/io/joo/tdd/pay/nontest/LoginService.java
@@ -1,27 +1,22 @@
 package io.joo.tdd.pay.nontest;
 
 
-import io.joo.tdd.pay.auth.AuthUtil;
-import io.joo.tdd.pay.auth.Customer;
-import io.joo.tdd.pay.auth.CustomerRepository;
-import io.joo.tdd.pay.auth.LoginResult;
+import io.joo.tdd.pay.auth.*;
 
 public class LoginService {
-    private String authKey = "somekey";
+    private AuthService authService = new AuthService();
     private CustomerRepository customerRepo;
 
     public LoginService(CustomerRepository customerRepo) {
         this.customerRepo = customerRepo;
     }
 
+    public void setAuthService(AuthService authService) {
+        this.authService = authService;
+    }
+
     public LoginResult login(String id, String pw) {
-        int resp = 0;
-        boolean authorized = AuthUtil.authorize(authKey);
-        if (authorized) {
-            resp = AuthUtil.authenticate(id, pw);
-        } else {
-            resp = -1;
-        }
+        int resp = authService.authenticate(id, pw);
         if (resp == -1) return LoginResult.badAuthKey();
 
         if (resp == 1) {
